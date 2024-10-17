3f22bd86c842720a2ff644fb1ba7f8f47f7ab3aa
MarekKoziak <240933@vutbr.cz>
2024-10-18 00:10:03 +0200

    Learning to draw simple shapes on the screen

--- OpenGL C++ projekt/Homogenous cordinates/Homogenous cordinates/src/Main.cpp
+++ OpenGL C++ projekt/Homogenous cordinates/Homogenous cordinates/src/Main.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+
+#include <fstream>
+#include <sstream>
+#include <streambuf>
+#include <string>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+std::string loadShaderSrc(const char* filename);
 
 int main() {
 
-	//glm test
-	glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
-	glm::mat4 trans = glm::mat4(1.0f);	//identity matrix
-	//trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));	//translation
-	trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));	//rotarion
-	trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));	//scaling
-
-	vec = trans * vec;
-	std::cout << vec.x << ' ' << vec.y << ' ' << vec.z << std::endl;
+	int success;
+	char infoLog[512];
 
 	glfwInit();
 
@@ -50,6 +46,124 @@ int main() {
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	/*
+		shaders
+	*/
+
+	// compile vertex shader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	std::string vertShaderSrc = loadShaderSrc("assets/vertex_core.glsl");
+	const GLchar* vertShader = vertShaderSrc.c_str();
+	glShaderSource(vertexShader, 1, &vertShader, NULL);
+	glCompileShader(vertexShader);
+
+	// catch error
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "Error with vertex shader comp.:" << std::endl << infoLog << std::endl;
+	}
+
+	// compile fragment shader
+	unsigned int fragmentShader[2];
+
+	fragmentShader[0] = glCreateShader(GL_FRAGMENT_SHADER);
+	std::string fragShaderSrc = loadShaderSrc("assets/fragment_core.glsl");
+	const GLchar* fragShader = fragShaderSrc.c_str();
+	glShaderSource(fragmentShader[0], 1, &fragShader, NULL);
+	glCompileShader(fragmentShader[0]);
+
+	//catch error
+	glGetShaderiv(fragmentShader[0], GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader[0], 512, NULL, infoLog);
+		std::cout << "Error with fragment shader comp.:" << std::endl << infoLog << std::endl;
+	}
+
+	fragmentShader[1] = glCreateShader(GL_FRAGMENT_SHADER);
+	fragShaderSrc = loadShaderSrc("assets/fragment_core2.glsl");
+	fragShader = fragShaderSrc.c_str();
+	glShaderSource(fragmentShader[1], 1, &fragShader, NULL);
+	glCompileShader(fragmentShader[1]);
+
+	//catch error
+	glGetShaderiv(fragmentShader[1], GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader[1], 512, NULL, infoLog);
+		std::cout << "Error with fragment shader comp.:" << std::endl << infoLog << std::endl;
+	}
+
+	// shader program
+	unsigned int shaderProgram[2];
+
+	shaderProgram[0] = glCreateProgram();
+
+	glAttachShader(shaderProgram[0], vertexShader);
+	glAttachShader(shaderProgram[0], fragmentShader[0]);
+	glLinkProgram(shaderProgram[0]);
+
+	// catch error
+	glGetProgramiv(shaderProgram[0], GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram[0] , 512, NULL, infoLog);
+		std::cout << "Linking error:" << std::endl << infoLog << std::endl;
+	}
+
+	shaderProgram[1] = glCreateProgram();
+
+	glAttachShader(shaderProgram[1], vertexShader);
+	glAttachShader(shaderProgram[1], fragmentShader[1]);
+	glLinkProgram(shaderProgram[1]);
+
+	// catch error
+	glGetProgramiv(shaderProgram[1], GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram[1], 512, NULL, infoLog);
+		std::cout << "Linking error:" << std::endl << infoLog << std::endl;
+	}
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader[0]);
+	glDeleteShader(fragmentShader[1]);
+
+	// vertex array
+	float vertices[] = {
+		 0.5f,  0.5f, 0.0f,	//top right
+		-0.5f,  0.5f, 0.0f,	//top left
+		-0.5f, -0.5f, 0.0f,	//bottom left
+		 0.5f, -0.5f, 0.0f	//bottom right
+	};
+	unsigned int indicies[] = {
+		0, 1, 2,	//first triangle
+		2, 3, 0		//second triangle
+	};
+
+
+	// VAO, VBO
+	unsigned int VAO, VBO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	// bind VAO
+	glBindVertexArray(VAO);
+
+	// bid VBO
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// set attribute pointer
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// set up EBO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
+
+
+
+
 	while (!glfwWindowShouldClose(window)) {
 		// process input
 		processInput(window);
@@ -58,10 +172,24 @@ int main() {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// draw shapes
+		glBindVertexArray(VAO);
+
+		// first triangle
+		glUseProgram(shaderProgram[0]);
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+		//second triangle
+		glUseProgram(shaderProgram[1]);
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void*)(3 * sizeof(unsigned int)));
+
 		// send new frame to window
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VAO);
+	glDeleteBuffers(1, &EBO);
 
 	glfwTerminate();
 	return 0;
@@ -75,4 +203,24 @@ void processInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
 	}
+}
+
+std::string loadShaderSrc(const char* filename) {
+	std::ifstream file;
+	std::stringstream buf;
+
+	std::string ret = "";
+
+	file.open(filename);
+
+	if (file.is_open()) {
+		buf << file.rdbuf();
+		ret = buf.str();
+	}else {
+		std::cout << "Could not open " << filename << std::endl;
+	}
+
+	file.close();
+
+	return ret;
 }
